e5899bb9799cfabe2b213232c5e4166f466ad2ca
NDanczuk <nicolasdanc@gmail.com>
2024-08-19 10:33:43 -0300

    Config GraphQL

---
+++ src/app.resolver.ts
@@ -0,0 +1,11 @@
+import { Get } from '@nestjs/common'
+import { AppService } from './app.service'
+import { Query, Resolver } from '@nestjs/graphql'
+
+@Resolver()
+export class AppResolver {
+  @Query(() => String)
+  hello(): string {
+    return 'Hello dev!'
+  }
+}
