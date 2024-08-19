521853a75f14b4c8e73aed5c0551137a48e4d8da
NDanczuk <nicolasdanc@gmail.com>
2024-08-19 11:28:20 -0300

    Config AuthorsPrismaRepository in AuthorsModule

--- src/authors/repositories/authors-prisma.repository.ts
+++ src/authors/repositories/authors-prisma.repository.ts
@@ -6,8 +6,13 @@ import {
 } from '../interfaces/authors.repository'
 import { ICreateAuthor } from '../interfaces/create-author'
 import { IUpdateAuthor } from '../interfaces/update-author'
+import { PrismaService } from '@/database/prisma/prisma.service'
 
 export class AuthorsPrismaRepository implements IAuthorsRepository {
+  sortableFields: string[] = ['name', 'email', 'createdAt']
+
+  constructor(private prisma: PrismaService) {}
+
   create(data: ICreateAuthor): Promise<Author> {
     throw new Error('Method not implemented.')
   }
