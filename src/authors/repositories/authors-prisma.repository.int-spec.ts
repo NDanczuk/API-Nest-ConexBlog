e06a132fe8733290559097aa3cf97edf76ab479f
NDanczuk <nicolasdanc@gmail.com>
2024-08-19 13:39:01 -0300

    Find all jest test added

---
+++ src/authors/repositories/authors-prisma.repository.int-spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { AuthorsPrismaRepository } from './authors-prisma.repository'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
+import { NotFoundError } from '@/shared/errors/not-found-error'
+
+describe('AuthorsPrismaRepository integration tests', () => {
+  let module: TestingModule
+  let repository: AuthorsPrismaRepository
+  const prisma = new PrismaClient()
+
+  beforeAll(async () => {
+    execSync('npm run prisma:migratetest')
+    await prisma.$connect()
+    module = await Test.createTestingModule({}).compile()
+    repository = new AuthorsPrismaRepository(prisma as any)
+  })
+
+  beforeEach(async () => {
+    await prisma.author.deleteMany()
+  })
+
+  afterAll(async () => {
+    await module.close()
+  })
+
+  // findById
+  test('should throw an error when the id is not found', async () => {
+    await expect(repository.findById('Wrong ID')).rejects.toThrow(
+      new NotFoundError('Author not found using ID Wrong ID'),
+    )
+  })
+})
