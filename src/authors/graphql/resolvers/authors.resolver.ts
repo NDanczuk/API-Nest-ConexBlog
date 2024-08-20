02fff6d80475b97531dc1eae8b253d601ac69330
NDanczuk <nicolasdanc@gmail.com>
2024-08-20 14:34:18 -0300

    List users query added

--- src/authors/graphql/resolvers/authors.resolver.ts
+++ src/authors/graphql/resolvers/authors.resolver.ts
@@ -1,13 +1,23 @@
-import { PrismaService } from '@/database/prisma/prisma.service'
-import { Query, Resolver } from '@nestjs/graphql'
+import { Args, Query, Resolver } from '@nestjs/graphql'
 import { Author } from '../models/author'
+import { Inject } from '@nestjs/common'
+import { ListAuthorsUsecase } from '@/authors/usecases/list-authors.usecase'
+import { SearchParamsArgs } from '../args/search-params.args'
+import { SearchAuthorsResult } from '../models/search-authors-result'
 
 @Resolver(() => Author)
 export class AuthorsResolver {
-  constructor(private prisma: PrismaService) {}
+  @Inject(ListAuthorsUsecase.Usecase)
+  private listAuthorUseCase: ListAuthorsUsecase.Usecase
 
-  @Query(() => [Author])
-  authors() {
-    return this.prisma.author.findMany()
+  @Query(() => SearchAuthorsResult)
+  authors(@Args() { page, perPage, sort, sortDir, filter }: SearchParamsArgs) {
+    return this.listAuthorUseCase.execute({
+      page,
+      perPage,
+      sort,
+      sortDir,
+      filter,
+    })
   }
 }
