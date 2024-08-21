b7e855e8fe21c69324286f992ee65a62301547e4
NDanczuk <nicolasdanc@gmail.com>
2024-08-21 11:37:06 -0300

    Get post usecase added

---
+++ src/posts/usecases/get-post.usecase.ts
@@ -0,0 +1,29 @@
+import { PostOutput } from '../dto/post-output'
+import { PostsPrismaRepository } from '../repositories/posts-prisma.repository'
+import { BadRequestError } from '@/shared/errors/bad-request-error'
+import slugify from 'slugify'
+import { ConflictError } from '@/shared/errors/conflict-error'
+
+export namespace GetPostUseCase {
+  export type Input = {
+    id: string
+  }
+
+  export type Output = PostOutput
+
+  export class UseCase {
+    constructor(private postsRepository: PostsPrismaRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const post = await this.postsRepository.findById(input.id)
+      return {
+        id: post.id,
+        title: post.title,
+        content: post.content,
+        published: post.published,
+        authorId: post.authorId,
+        createdAt: post.createdAt,
+      }
+    }
+  }
+}
